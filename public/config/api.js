dc2e5d86477f6d838a10b1f87c95538cbbfa4f18
kaiappelt <kaiappelt53@gmail.com>
2021-12-17 21:13:35 -0300

    primeiro commit

---
+++ public/config/api.js
@@ -0,0 +1,21 @@
+const baseURL = "";
+
+const api = axios.create({
+    baseURL,
+    headers: {
+        "Accept": "application/json",
+        "Content": "application/json"
+    }
+});
+
+// api.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token");
+
+//     if(token) {
+//         config.headers.Authorization = `Bearer ${token}`
+//     }
+
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
